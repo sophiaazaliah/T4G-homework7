036b10039b2c7e7e03d20f4f8a621a3c08bae14a
Sophiaazaliah <Sophiaazaliah10@gmail.com>
2024-12-09 13:21:11 +0000

    changes

---
+++ src/index.js
@@ -0,0 +1,46 @@
+let username =prompt("what is your name?");
+let age =prompt("how old are you?");
+let email =prompt("write your email");
+
+ if (age < 12)  {
+ alert(`hi ${name}, you are ${age}years old and you are
+too young to register.sorry 😑😑`);   
+ }else if(12 < age < 18) {
+ alert (`hi ${name}, you are ${age} years old and you have
+    limited option to register for.We will keep in touch
+    via your email: ${email}🖐🖐`);   
+ }else if (age > 18) {
+    alert(`hi ${name},you are ${age} years old and you can
+    register for any option of your choosing.We will keep in
+    touch via your email: ${email}🖐`);
+ }else{
+    alert("please enter a valid age");
+ }
+  
+
+ 
+
+
+  
+  
+ 
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
